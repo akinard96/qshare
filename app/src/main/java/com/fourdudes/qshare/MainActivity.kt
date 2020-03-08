b6a983771a505ebeb1aa93fd92ac22225644262a
Caleb Dudley <calebdudley@mines.edu>
2020-03-07 17:44:19 -0700

    Added FAB and actions, added speed dial library, added TODOs to hook into activities

--- app/src/main/java/com/fourdudes/qshare/MainActivity.kt
+++ app/src/main/java/com/fourdudes/qshare/MainActivity.kt
@@ -5,12 +5,37 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import com.leinardi.android.speeddial.SpeedDialActionItem
+import com.leinardi.android.speeddial.SpeedDialView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val speedDialView = findViewById<SpeedDialView>(R.id.speed_dial)
+        speedDialView.inflate(R.menu.fab_actions_menu)
+        speedDialView.setOnActionSelectedListener(SpeedDialView.OnActionSelectedListener { actionItem ->
+            when(actionItem.id) {
+                R.id.upload_fab -> {
+                    //TODO present file picker
+                    speedDialView.close()
+                    true
+                }
+                R.id.scan_fab -> {
+                    //TODO start scan code activity
+                    speedDialView.close()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        })
+//        speedDialView.addActionItem(
+//            SpeedDialActionItem.Builder(R.id.fab_no_label, R.drawable.ic_link_white_24dp)
+//                .create())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,15 +47,15 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.about_menu_item -> {
-                //start about activity
+                //TODO start about activity
                 true
             }
             R.id.help_menu_item -> {
-                //start help activity
+                //TODO start help activity
                 true
             }
             R.id.settings_menu_item -> {
-                //start settings activity
+                //TODO start settings activity
                 true
             }
             else -> super.onOptionsItemSelected(item)
