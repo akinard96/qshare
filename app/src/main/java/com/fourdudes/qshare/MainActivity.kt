6826b03f5074dd73c109d31c6b79cc7a13ae9a07
Caleb Dudley <calebdudley@mines.edu>
2020-03-07 17:10:27 -0700

    Added menu and items

--- app/src/main/java/com/fourdudes/qshare/MainActivity.kt
+++ app/src/main/java/com/fourdudes/qshare/MainActivity.kt
@@ -2,11 +2,38 @@ package com.fourdudes.qshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_activity_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.about_menu_item -> {
+                //start about activity
+                true
+            }
+            R.id.help_menu_item -> {
+                //start help activity
+                true
+            }
+            R.id.settings_menu_item -> {
+                //start settings activity
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
