d510730e78ad28f5df2c6fc1e6d10ca551315ddd
invee123@gmail.com <$$Invee257$$>
2020-03-08 20:59:39 -0600

    Added Search and Scan Basic layout and packages

---
+++ app/src/main/java/com/fourdudes/qshare/Scan/ScanActivity.kt
@@ -0,0 +1,12 @@
+package com.fourdudes.qshare.Scan
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fourdudes.qshare.R
+
+class ScanActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
